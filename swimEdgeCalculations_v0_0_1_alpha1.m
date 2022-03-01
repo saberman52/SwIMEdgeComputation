0722e3c97fd73e2c8e50a2faa3b05018b0c5f0f8
Simon Berman <saberman52@gmail.com>
2022-03-01 12:21:10 -0800

    Added v0=0.1 and alpha=1 trajectory calculations and plotting code.

--- swimEdgeCalculations_v0_0_1_alpha1.m
+++ swimEdgeCalculations_v0_0_1_alpha1.m
@@ -90,3 +90,85 @@ set(gca,'FontSize',font)
 ylim([-0.05,0.55])
 xlim([-0.05,0.55])
 
+%% compute BIM for same fixed point
+aBim = -1;
+[V,D] = eig(flow.A(q_eq(1),q_eq(2),q_eq(3),v0,aBim,0));
+[~,indMax] = max(diag(D));
+bimDir = V(:,indMax);
+epsilon = 1e-3;
+if bimDir(1) < 0
+    bimDir = -bimDir;
+end
+qS0 = q_eq.' + epsilon*bimDir;
+tspan = [0 Tf];
+tols = 1e-9*[1,1];
+[Xs,Ys,THs,Ts] = swimmerTrajectoryU(qS0(1),qS0(2),qS0(3),tspan,flow,v0,aBim,tols);
+
+%% calculate trajectories to illustrate blocking behavior
+n=1e4;
+tf=3;
+% initialize trajectories on line y = 0, with theta in (0,pi) so everyone
+% swimming up
+X0 = 1*rand([n,1]) - 0.5;
+Y0 = zeros([n,1]);
+TH0 = pi*rand([n,1]);
+tols = [1e-9,1e-9];
+[X,Y,TH,T] = swimmerTrajectoryU(X0,Y0,TH0,[0,tf],flow,v0,alpha,tols);
+
+%% plot trajectories illustrating blocking behavior, in two panels with different cutoff times
+% collate folds into single curve: the SwIM edge
+    % plot computed SwIM edge
+Xswim = []; Yswim = []; THswim = [];
+    for i = 1:length(Ti)
+        switch i
+            case 1
+                inds = 1:34;
+            case 2
+                inds = 10:17;
+            case 3
+                inds = 15:29;
+            case 4
+                inds = 10:length(Xfc{4});
+        end
+        Xswim = [Xswim; Xfc{i}(inds)];
+        Yswim = [Yswim; Yfc{i}(inds)];
+        THswim = [THswim;atan2(nfc{i}(inds,2),nfc{i}(inds,1))];
+    end
+
+figure
+for iter = 1:2
+    subplot(1,2,iter)
+    % plot BIM
+    plotSwimmerTrajectory(Xs,Ys,THs,v0,alpha,flow,1,'r:')
+    hold on
+    % plot reflection of BIM
+    plotSwimmerTrajectory(-Xs,Ys,pi-THs,v0,alpha,flow,0,'r:')
+    hold on
+    % plot computed SwIM edge
+    plotSwimmerTrajectory(Xswim,Yswim,THswim,v0,alpha,flow,0,'r',0)
+    hold on
+    % plot reflection
+    plotSwimmerTrajectory(-Xswim,Yswim,pi-THswim,v0,alpha,flow,0,'r',0)
+    hold on
+    
+    if iter == 1
+        tcut = 1.66;
+    else
+        tcut = tf;
+    end
+    % plot trajectories
+    plot(X(T <= tcut,:),Y(T <= tcut,:),'g')
+    if iter == 1
+        label = ['(a) $t \leq ',num2str(tcut),'$'];
+    else
+        label = ['(b) $t \leq ',num2str(tcut),'$'];
+    end
+    text(-0.9,0.6,label,'Interpreter','latex','FontSize',font)
+    
+    ylim([-0.1,0.6])
+    xlim([-1,1])
+    set(gca,'YTick',[0,0.5],'XGrid','on','YGrid','on','GridAlpha',0.6,'FontSize',font)
+end
+
+% ylim([-0.05,0.55])
+% xlim([-0.05,0.55])
